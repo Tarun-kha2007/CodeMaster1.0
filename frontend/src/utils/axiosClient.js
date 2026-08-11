9edfa2f4acd52eb8389c0d119b4107564a582d74
Tarun-kha2007 <tarunk.dd24.cs@gmail.com>
2026-08-12 00:23:43 +0530

    Fix baseURL assignment in axiosClient

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosClient =  axios.create({
-    baseURL: 'https://codemaster1-0.onrender.com || http://localhost:3000',
+    baseURL: 'https://codemaster1-0.onrender.com' || 'http://localhost:3000',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
